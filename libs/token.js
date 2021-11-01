f9cd741c35634e53044ea1506026cd9508df266d
sally0226 <sally734595@gmail.com>
2021-11-02 00:56:49 +0900

    Fix: jwtmiddleware 임시 수정
    - cookie -> authorization header 사용
    - 재발급 기능은 보류

--- libs/token.js
+++ libs/token.js
@@ -11,21 +11,21 @@ const decodeToken = (token) => {
     return jwt.verify(token, jwtSecretKey);
 }
 const jwtMiddleware = (req, res, next) => {
-    const token = req.cookies.access_token;
+    const token = req.headers.authorization;
     // token이 없으면 바로 return
     if (token === undefined){
         next(); return; 
     }
     try { 
         const decoded = decodeToken(token);
-        const renewMin = 10; // 토큰 만료시간이 renewMin분 이하이면 재발급
-        if (decoded.exp - Date.now()/1000 < renewMin*60) {
-            const { user_id } = decoded;
-            const freshToken = createToken({user_id});
-            res.cookie('access_token', freshToken, {
-                httpOnly: true, sameSite: 'none', secore: true
-            });
-        }
+        // const renewMin = 10; // 토큰 만료시간이 renewMin분 이하이면 재발급
+        // if (decoded.exp - Date.now()/1000 < renewMin*60) {
+        //     const { user_id } = decoded;
+        //     const freshToken = createToken({user_id});
+        //     res.cookie('access_token', freshToken, {
+        //         httpOnly: true, sameSite: 'none', secore: true
+        //     });
+        // }
         req.user = decoded.user_id; 
     }catch(err){
         // token validation 실패 
