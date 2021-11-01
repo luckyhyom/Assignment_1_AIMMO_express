410294c521042542379574699cd18119d42e9b70
sally0226 <sally734595@gmail.com>
2021-11-02 02:17:03 +0900

    Feat: Comment Model / mongoDB auto-increment 적용, create함수 작성
    - mongoose-auto-increment 라이브러리 설치 후 이용
    - create 함수 작성

--- Models/CommentModel.js
+++ Models/CommentModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+
+autoIncrement.initialize(mongoose.connection);
 
 const commentSchema = mongoose.Schema({
     commentId: {type: Number, required: true, unique: true, trim: true},
@@ -6,9 +9,25 @@ const commentSchema = mongoose.Schema({
     boardId: {type: Number, ref: 'board'},
     parentId: {type: Number, ref: 'comment'},
     depth: {type: Number, required: true, default: 1},
-    contents: {type: String, required: true, trim: true},
+    content: {type: String, required: true, trim: true},
     createdDt: {type: Date, required: true},
     updatedDt: {type: Date, required: true},
 });
 
+commentSchema.plugin(
+    autoIncrement.plugin,
+    {
+         model : 'comment', // line 30에 모델명 있음 
+         field : 'commentId',
+         startAt : 1, //시작
+         increment : 1 // 증가
+    });
+commentSchema.statics.create = function(boardId, content, depth, parentId, userId){
+    const comment = new this({
+        boardId, content, depth, parentId, userId,
+        createdDt: Date.now(),
+        updatedDt: Date.now()
+    });
+    return comment.save();
+}
 module.exports = mongoose.model('comment', commentSchema);
