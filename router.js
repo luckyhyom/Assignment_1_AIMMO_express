e6f586fa48eaa5cea9ffe9ed4910c10aa3971542
sally0226 <sally734595@gmail.com>
2021-11-02 16:10:20 +0900

    Feat: Comment API 작성
    - 댓글, 대댓글 관련 CRUD API 작성
    - Read시에 pagination 적용

--- router.js
+++ router.js
@@ -3,22 +3,20 @@ const router = express.Router();
 
 // import controllers
 const userController = require('./Controllers/userController');
+const commentController = require('./Controllers/CommentController');
 
-router.post('/user', userController.join); // 회원가입
+router.post('/signup', userController.signup); // 회원가입
 router.post('/signin', userController.signin); // 로그인
-router.get ('/signout', ); // 로그아웃
 
 router.post('/board', ); // 게시글 생성
 router.patch('/board', ); // 게시글 수정
 router.delete('/board/:id', ); // 게시글 삭제
-router.get('/board/list', ); // 게시글 목록 가져오기 pagination
+router.get('/board', ); // 게시글 목록 가져오기 pagination / 검색
 router.get('/board/:id', ); // 게시글 내용 가져오기
-router.get('board/search', ) //
 
-router.post('/comment', ); // 댓글,대댓글 생성
-router.delete('/comment/:id', ) // 댓글,대댓글 삭제
-router.patch('/comment/:id', ); // 댓글, 대댓글 수정
-router.get('/comment/:boardId', ); // 댓글 읽기 pagination 
-router.get('/comment/:commentId', ) // 대댓글 읽기 pagination
+router.post('/comment', commentController.writeComment); // 댓글,대댓글 생성
+router.delete('/comment/:id', commentController.deleteComment) // 댓글,대댓글 삭제
+router.patch('/comment/:id', commentController.modifyComment); // 댓글, 대댓글 수정
+router.get('/comment', commentController.getCommentList); // 댓글, 대댓글 읽기 pagination 
 
 module.exports = router; 
