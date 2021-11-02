b5a95d02f5279f66847d28e3ca217feeb18633cb
heejin99 <wkdgmlwls30@naver.com>
2021-11-02 20:27:54 +0900

    Fix: Board API 조회수/ 카테고리 검색 기능 추가 및 error 수정
    - Board API 조회수 기능
    - Board API 카테고리 검색 기능
    - Board API 접근 시 발생하는 Error 수정

--- Controllers/boardController.js
+++ Controllers/boardController.js
@@ -3,33 +3,35 @@ const board = require('../Models/BoardModel')
 const writeBoard = async function(req, res, next) {
     try {
         const { title, contents, categoryCode} = req.body
-        const userId = req.user.userId
+        const userId = req.user?.userId
         if (!userId) throw new Error("로그인이 필요합니다.")
         await board.create(title, contents, categoryCode, userId)
         return res.status(201).json({
             success: true,
-            message: "게시글이 등록되었습니다.",
-            board
+            message: "게시글이 등록되었습니다."
         })
-    } catch (e) {
-        if (e.code === 11000) {
-            e.status = 401
-            e.message = "로그인이 필요합니다."
-        }
-        next(e)
+    } catch (err) {
+        if (err.message === "로그인이 필요합니다.")
+            err.status = 401
+        next(err)
     }
 }
 
 const modifyBoard = async function(req, res, next) {
     try {
-        const userId = req.user.userId
-        const userInfo = await board.find({ userId })
-        if (!userInfo) throw new Error("권한이 없습니다.") 
-
         const {title, contents, categoryCode} = req.body
+        
+        const userId = req.user?.userId
+        const boardId = req.params.id
+
+        if (!userId) throw new Error("권한이 없습니다.") 
+
+        const userInfo = board.findOne({boardId})
+        if (userId !== userInfo) throw new Error("권한이 없습니다.")
+
         const boardInfo = await board.updateOne(
             {
-                boardId: req.params.id,
+                boardId,
                 userId
             },
             { $set : { title, updatedDt: Date.now(), contents, categoryCode }}
@@ -41,35 +43,43 @@ const modifyBoard = async function(req, res, next) {
             message: "수정되었습니다.", 
             boardInfo
         })
-    } catch (e) {
-        if (e.message === "권한이 없습니다.")
-            e.status = 403
-        next(e)
+    } catch (err) {
+        if (err.message === "권한이 없습니다.")
+            err.status = 403
+        if (err.message === "존재하지 않는 게시글입니다.")
+            err.status = 404   
+        next(err)
     }
 }
 
 const deleteBoard = async function (req, res, next) {
     try {
-        const userInfo = await board.find({ userId })
-        if (!userInfo) throw new Error("권한이 없습니다.") 
+        const userId = req.user?.userId
+        const boardId = req.params.id
+        if (!userId) throw new Error("권한이 없습니다.") 
 
-        const boardInfo = await board.deleteOne({ boardId: req.params.id, userId: req.user.userId})
+        const userInfo = board.findOne({boardId})
+        if (userId !== userInfo.userId) throw new Error("권한이 없습니다.")
+
+        const boardInfo = await board.deleteOne({ boardId, userId})
         if(!boardInfo) throw new Error("존재하지 않는 게시글입니다.")
 
         res.status(200).json({
             success: true, 
-            message: "삭제되었습니다.", 
-            boardInfo
+            message: "삭제되었습니다."
         })
-    } catch (e) {
-        next(e)
-        
+    } catch (err) {
+        if (err.message==="권한이 없습니다.")
+            err.status = 403
+        else if (err.message==="존재하지 않는 게시글입니다.")
+            err.status = 404
+        next(err)
     }
 }
 
-const listBoard = async function (req, res) {
+const listBoard = async function (req, res, next) {
     try {
-        const {title, author, pageNo} = req.query
+        const {title, author, category, pageNo} = req.query
         var postQuery = []
         var orQuery = {}
 
@@ -81,12 +91,12 @@ const listBoard = async function (req, res) {
             postQuery.push({title: {$regex: title}})
         } else if (author) {
             postQuery.push({userId: {$regex: author}})
+        } else if (category) {
+            postQuery.push({categoryCode: parseInt(category)})
         }
-
         if (postQuery) {
             orQuery = {$or: postQuery}
         }
-
         const boardInfo = await board.find(orQuery)
             .sort({ updatedDt: -1 }) // 업데이트된 날짜로 내림차순
             .skip(parseInt(skipSize))
@@ -94,40 +104,57 @@ const listBoard = async function (req, res) {
             .populate("_id")
             .exec()
         
+        if (!boardInfo) throw new Error("존재하지 않는 페이지입니다.")
+
         res.status(200).json({
             success: true,
             message:"성공했습니다.", 
             skipSize,
             boardInfo
         })
-        
-    } catch (e) {
-        console.log(e)
-        res.status(404).json({
-            success: false,
-            message: "존재하지 않는 페이지입니다."
-        })
+    } catch (err) {
+        if (err.message === "존재하지 않는 페이지입니다.")
+            err.status = 404
+        next(err)
     }
 }
 
-const detailBoard = async function (req, res) {
+const detailBoard = async function (req, res, next) {
     try {
-        const userId = req.user.userId
+        const userId = req.user?.userId
+        const boardId = req.params.id
+        let objOriginal = {}
         const boardInfo = await board.findOne({ 
-            boardId: req.params.id, 
-            userId
+            boardId
         })
         if (!boardInfo) throw new Error("존재하지 않는 게시글입니다.")
-        res.status(200).json({
-            boardInfo
-        })
-    } catch (e) {
-        console.log(e)
-        if (e.message === "존재하지 않는 게시글입니다.")
-            res.status(404).json({
-                success: false,
-                message: e.message
+
+        if (userId !== undefined) {
+            if (boardInfo.readUser !== undefined) {
+                objOriginal = boardInfo.readUser
+            }
+            objOriginal[userId] = true
+            
+            await boardInfo.updateOne(
+                {
+                $set: {
+                    readUser: objOriginal
+                }
+            }, {
+                upsert: true
             })
+    
+        }
+        const cnt = Object.keys(objOriginal).length
+        res.status(200).json({
+            boardInfo,
+            cnt
+        })
+
+    } catch (err) {
+        if (err.message === "존재하지 않는 게시글입니다.")
+            err.status = 404
+        next(err)
     }
 }
 
